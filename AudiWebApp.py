a95b194a06886aefc542316d298fcb20e0e93f86
Jorge Ramos V <chito.012008@hotmail.com>
2022-04-27 22:57:51 -0500

    ok

--- AudiWebApp.py
+++ AudiWebApp.py
@@ -1,3 +1,5 @@
+from curses.ascii import alt
+import re
 from flask import Flask
 
 
@@ -32,6 +34,9 @@ except Exception as e:
     print("Database connection failed due to {}".format(e))"""
 
 
+
+
+
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == "GET":
@@ -42,38 +47,144 @@ def login():
         password=request.form['password']
 
         print("Hola, este es un mensaje en consola")
-        print(usuario)
-        print(password)
+        print(usuario, password)
         return render_template("home.html")
+
+
+
 
 
 @app.route('/registrar_usuario', methods=['GET', 'POST'])
 def registrar_usuario():
     if request.method == 'GET':
         return render_template("registrousuarios.html")
+    else:
+        id=request.form('idUsuario')
+        nombre=request.form('nombreUsuario')
+        nombreUsuario=request.form('nombreUsuario')
+        password=request.form('password')
+        confirmPassword=request.form('password')
+        
+        print("Hola, ese es registro usuarios")
+        print(id,nombre,nombreUsuario,password,confirmPassword)
+        return render_template("registrousuarios.html")
+        
+    
+    
+    
+    
         
 
 @app.route('/registro_productos', methods=['GET', 'POST'])
 def registro_productos():
     if request.method == 'GET':
         return render_template("registrousuarios.html")
+    else:
+        usuario=request.form('username')
+        password=request.form('password')
+        
+        print("Hola, este es registroproductos")
+        print(usuario, password)
+        return render_template('registrousuarios.html')
+    
+    
+    
+    
+    
 
 @app.route('/registrar_proveedor', methods=['GET', 'POST'])
 def registrar_proveedor():
     if request.method == 'GET':
         return render_template("registroproveedores.html")
+    else:
+        id=request.form('idProveedor')
+        nombre=request.form('nombreProveedor')
+        cp=request.form('codigopostal')
+        area=request.form('area')
+        zona=request.form('zona')
+        ciudad=request.form('ciudad')
+        estado.request.form('estado')
+        pais.request.form('pais')
+        contraseña=request.form('password')
+        confirmarContraseña=request.form('password')
+        
+        print("Hola, este es registro proveedores")
+        print(id,nombre,cp,area,zona,ciudad,estado,pais,contraseña,confirmarContraseña)
+        return render_template('registrousuarios.html')
+    
+    
+    
+    
+    
+    
 
 @app.route('/registrar_partes', methods=['GET', 'POST'])
 def registrar_partes():
     if request.method == 'GET':
         return render_template("registropiezas.html")  
+    else:
+        id=request.form('idParte')
+        numParte=request.form('numeroParte')
+        nombreParte=request.form('nombreParte')
+        idProveedor=request.form('idProveedor')
+        altoPieza=request.form('altoPieza')
+        largoPieza=request.form('largoPieza')
+        anchoPieza=request.form('anchoPieza')
+        pesoPieza=request.form('pesoPieza')
+        ebr=request.form('ebr')
+        tiempoProveedor=request.form('tiempoProveedor')
+        empaquesug=request.form('empaquesug')
+        piezasporcont=request.form('piezasporcont')
+        
+        print("Hola, este es registro piezas")
+        print(id,numParte,idProveedor,
+              altoPieza,largoPieza,anchoPieza,
+              pesoPieza,ebr,tiempoProveedor,
+              empaquesug,piezasporcont)
+        return render_template('registropiezas.html')
+    
+    
+    
+    
     
 @app.route('/registrar_reporte', methods=['GET', 'POST'])
 def registrar_reporte():
     if request.method == 'GET':
         return render_template("incidencias.html")    
+    
+    else:
+        fechareporte=request.form('fechareporte')
+        reporte=request.form('reporte')
+        alto=request.form('alto')
+        comentarios=request.form('comentarios')
+        
+        print("Hola, este es incidencias")
+        print(fechareporte,reporte,alto,comentarios)
+        return render_template('registrousuarios.html')
+    
+    
+
+
 
 @app.route('/registrar_contenedor', methods=['GET', 'POST'])
 def registrar_contenedor():
     if request.method == 'GET':
         return render_template("registrocontenedores.html")      
+    else:
+       idcontenedor=request.form('idcontenedor')
+       codigocontenedor=request.form('codigocontenedor')
+       nombrecontenedor=request.form('nombrecontenedor')
+       alto=request.form('alto')
+       largo=request.form('largo')
+       ancho=request.form('ancho')
+       capacidad=request.form('capacidad')
+       costocontenedor=request.form('costocontenedor')
+       tipomaterial=request.form('tipomaterial')
+       
+    print("hola, este es registro contenedores")    
+    print(idcontenedor,codigocontenedor,nombrecontenedor,
+              alto,largo,ancho,capacidad,costocontenedor,tipomaterial)
+    return render_template('registrocontenedores.html')
+    
+    
+    
